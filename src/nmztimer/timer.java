4e6d2e1400c72fa51991b5506349cac0a1764429
reprise5 <rzqxx5gv@gmail.com>
2016-07-31 11:23:08 -0400

    reset button fixed, notes combobox selectedIndex.

--- src/nmztimer/timer.java
+++ src/nmztimer/timer.java
@@ -25,17 +25,17 @@ public class timer extends javax.swing.JFrame {
     private void initComponents() {
 
         potionTypeTabbedPane = new javax.swing.JTabbedPane();
-        jPanel1 = new javax.swing.JPanel();
+        PrayerPotionPanel = new javax.swing.JPanel();
         dosageCombobox = new javax.swing.JComboBox();
         GObutton = new javax.swing.JButton();
         RESETbutton = new javax.swing.JButton();
         ProgressLabel = new javax.swing.JLabel();
         timeProgressBar = new javax.swing.JProgressBar();
         screen = new javax.swing.JTextField();
-        jPanel3 = new javax.swing.JPanel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        nothingPanel = new javax.swing.JPanel();
+        nothingpasswordfield = new javax.swing.JPasswordField();
+        nothingusernamefield = new javax.swing.JTextField();
+        nothingbutton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("RuneScape ppot timer");
@@ -49,7 +49,7 @@ public class timer extends javax.swing.JFrame {
         potionTypeTabbedPane.setToolTipText("Counts down doses of super strength & super attack potions.");
         potionTypeTabbedPane.setPreferredSize(new java.awt.Dimension(331, 174));
 
-        jPanel1.setToolTipText("Counts down doses of prayer potion");
+        PrayerPotionPanel.setToolTipText("Counts down doses of prayer potion");
 
         dosageCombobox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1 Dose", "2 Doses" }));
 
@@ -77,39 +77,39 @@ public class timer extends javax.swing.JFrame {
         screen.setDisabledTextColor(new java.awt.Color(204, 0, 0));
         screen.setFocusable(false);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout PrayerPotionPanelLayout = new javax.swing.GroupLayout(PrayerPotionPanel);
+        PrayerPotionPanel.setLayout(PrayerPotionPanelLayout);
+        PrayerPotionPanelLayout.setHorizontalGroup(
+            PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PrayerPotionPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(timeProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PrayerPotionPanelLayout.createSequentialGroup()
+                        .addGroup(PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(ProgressLabel)
                             .addComponent(screen, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(24, 24, 24)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(PrayerPotionPanelLayout.createSequentialGroup()
                                 .addComponent(GObutton, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(RESETbutton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .addComponent(dosageCombobox, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        PrayerPotionPanelLayout.setVerticalGroup(
+            PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PrayerPotionPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PrayerPotionPanelLayout.createSequentialGroup()
                         .addComponent(dosageCombobox, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addGroup(PrayerPotionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(GObutton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(RESETbutton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGroup(PrayerPotionPanelLayout.createSequentialGroup()
                         .addComponent(screen, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(ProgressLabel)))
@@ -118,48 +118,48 @@ public class timer extends javax.swing.JFrame {
                 .addContainerGap(23, Short.MAX_VALUE))
         );
 
-        potionTypeTabbedPane.addTab("Prayer Potion", null, jPanel1, "");
+        potionTypeTabbedPane.addTab("Prayer Potion", null, PrayerPotionPanel, "");
 
-        jPasswordField1.setText("Password");
+        nothingpasswordfield.setText("Password");
 
-        jTextField1.setText("Username");
+        nothingusernamefield.setText("Username");
 
-        jButton1.setText("Go?");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        nothingbutton.setText("Go?");
+        nothingbutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                nothingbuttonActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+        javax.swing.GroupLayout nothingPanelLayout = new javax.swing.GroupLayout(nothingPanel);
+        nothingPanel.setLayout(nothingPanelLayout);
+        nothingPanelLayout.setHorizontalGroup(
+            nothingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nothingPanelLayout.createSequentialGroup()
+                .addGroup(nothingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(nothingPanelLayout.createSequentialGroup()
                         .addGap(84, 84, 84)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(nothingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(nothingpasswordfield, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(nothingusernamefield, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(nothingPanelLayout.createSequentialGroup()
                         .addGap(112, 112, 112)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(nothingbutton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(92, Short.MAX_VALUE))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        nothingPanelLayout.setVerticalGroup(
+            nothingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nothingPanelLayout.createSequentialGroup()
                 .addGap(16, 16, 16)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(nothingusernamefield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(nothingpasswordfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addComponent(nothingbutton)
                 .addContainerGap(47, Short.MAX_VALUE))
         );
 
-        potionTypeTabbedPane.addTab("Nothing", jPanel3);
+        potionTypeTabbedPane.addTab("Nothing", nothingPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -183,8 +183,10 @@ public class timer extends javax.swing.JFrame {
 
     //reset the clock based on dosagebox selected index.
     private void RESETbuttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RESETbuttonActionPerformed
+        System.out.println("Resetting early...");
+        dosage = dosageCombobox.getSelectedIndex();
         if (dosage == 0){
-             seconds = 82;
+            seconds = 82;
             ticks = 0;
              
             sSeconds = Integer.toString(seconds);
@@ -208,7 +210,7 @@ public class timer extends javax.swing.JFrame {
 
         if (dosage == 0){
             seconds = 82;
-            System.out.println(" Counting 1 dose of Prayer Potion for " + seconds + " seconds." +  "   Selected_index: " + dosage);
+            System.out.println("Counting 1 dose of Prayer Potion for " + seconds + " seconds." +  "   Selected_index: " + dosage);
             screen.setText("82");
 
             //Set properties of the progress bar.
@@ -246,7 +248,7 @@ public class timer extends javax.swing.JFrame {
                         seconds = 82;
                         ticks = 0;
 
-                        System.out.println(seconds);
+                        System.out.println("Resetting...");
                         sSeconds = Integer.toString(seconds);
                         screen.setText(sSeconds);
                         timeProgressBar.setValue(ticks);
@@ -256,7 +258,7 @@ public class timer extends javax.swing.JFrame {
         }
         else if (dosage == 1){
             seconds = 165;
-            System.out.println(" Counting 2 doses of Prayer Potion for " + seconds + " seconds." +  " Selected_index: " + dosage);
+            System.out.println("Counting 2 doses of Prayer Potion for " + seconds + " seconds." +  " Selected_index: " + dosage);
             screen.setText("165");
 
             //Set properties of the progress bar.
@@ -278,7 +280,6 @@ public class timer extends javax.swing.JFrame {
                     //times up, ALERT ME to drink another potion.
                     if (seconds == 0){
                         screen.setText("TIME!");
-                        System.out.println(seconds);
 
                         //alert(alert);
 
@@ -293,7 +294,6 @@ public class timer extends javax.swing.JFrame {
                         seconds = 165;
                         ticks = 0;
 
-                        System.out.println(seconds);
                         sSeconds = Integer.toString(seconds);
                         screen.setText(sSeconds);
                         timeProgressBar.setValue(ticks);
@@ -303,18 +303,17 @@ public class timer extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_GObuttonActionPerformed
 
-    //Because I'm bored and wanted another tab for now till I have a legitimate idea on how to automate more RuneScape Stuff I do.
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    //Because I'm bored and wanted another tab for now till I have a legitimate idea on how to automate more RuneScape Stuff I do.   
+    private void nothingbuttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nothingbuttonActionPerformed
         var++;
-        
         if (var == 5){
             JOptionPane.showConfirmDialog(
-            rootPane,
-            "Stop pressing that button.\nIt doesn't do anything.\nMoron." 
+                rootPane,
+                "Stop pressing that button.\nIt doesn't do anything.\nMoron."
             );
             var = 0;
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_nothingbuttonActionPerformed
 
     //Sub that tries to play an audio stream when the timer hits 0.  it fails to do so
     public void alert(File alert){
@@ -366,14 +365,14 @@ public class timer extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton GObutton;
+    private javax.swing.JPanel PrayerPotionPanel;
     private javax.swing.JLabel ProgressLabel;
     private javax.swing.JButton RESETbutton;
     private javax.swing.JComboBox dosageCombobox;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JPanel nothingPanel;
+    private javax.swing.JButton nothingbutton;
+    private javax.swing.JPasswordField nothingpasswordfield;
+    private javax.swing.JTextField nothingusernamefield;
     private javax.swing.JTabbedPane potionTypeTabbedPane;
     private javax.swing.JTextField screen;
     private javax.swing.JProgressBar timeProgressBar;
